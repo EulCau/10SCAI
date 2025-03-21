eb79660565bfbf2dabbee759b1a6fafa9a844d7c
EulCau <lx_0318@mail.ustc.edu.cn>
2025-03-21 15:51:06 +0800

    Delete Project directory

--- Project/rfm.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-// File: rfm.h
-// Author: Liu Xing
-// Date: 2025/1/22
-// Invironment: witten with libtorch-2.5.0+cu12.4
-// Description: This is an implementation of RFM using libtorch.ISO/IEC 14882:2020 (std:c++20) is needed.
-
-#include "geometry.h"
