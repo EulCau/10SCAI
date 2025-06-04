a7bac9a3a3d75a20770f12f9ec26c6321431d73d
EulCau <lx_0318@mail.ustc.edu.cn>
2025-06-04 21:37:33 +0800

    Create Deep BSDE main for test

---
+++ cppRFM/implement/DeepBSDE/src/main.cpp
@@ -0,0 +1,21 @@
+#include "equation_factory.h"
+#include "config.h"
+#include <iostream>
+
+int main()
+{
+    Config cfg = load_config("hjb_lq_d100.json");
+
+    try
+    {
+        auto eqn = EquationFactory::instance().create("HJBLQ", cfg.eqn_config);
+        auto [dw, x] = eqn->sample(2);
+        std::cout << "Sample success. Shape of x: " << x.sizes() << std::endl;
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << "Failed to create equation: " << e.what() << std::endl;
+    }
+
+    return 0;
+}
