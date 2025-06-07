d3556931f11327c0f1a2476feadb3ba3d43c7e7d
EulCau <lx_0318@mail.ustc.edu.cn>
2025-06-07 13:46:19 +0800

    Update main.cpp

--- cppRFM/implement/DeepBSDE/src/main.cpp
+++ cppRFM/implement/DeepBSDE/src/main.cpp
@@ -7,7 +7,7 @@ int main()
 {
 	const Config cfg = load_config("hjb_lq_d100.json");
 
-	auto bsde = EquationFactory::instance().create(cfg.eqn_config.eqn_name, cfg.eqn_config);
+	const auto bsde = EquationFactory::instance().create(cfg.eqn_config.eqn_name, cfg.eqn_config);
 	BSDESolver solver(cfg, bsde);
 	std::cout << "Starting training..." << std::endl;
 	solver.train();
